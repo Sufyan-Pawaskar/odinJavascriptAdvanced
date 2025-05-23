d015e79ccaf53449fc9ac12f7dc8b79ed6c676bd
Sufyan-Pawaskar <125754147+Sufyan-Pawaskar@users.noreply.github.com>
2025-05-23 15:33:56 +0000

    updated the gamelogic by introducing the game controller further the win conditions needs to be updated correctly, currently not working as expected. Also needs to update the controller to reprompt the same user if tried entering a already checked cell on board.

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,6 +1,6 @@
 function createPlayer(playerName, playerNumber){
-    let name = playerName;
-    let number = playerNumber;
+    const name = playerName;
+    const number = playerNumber;
     let playerResult = false;
     let playerMarker = "";
     const getResult = function(){
@@ -17,17 +17,22 @@ function createPlayer(playerName, playerNumber){
     }
     return {name,number,getResult,updatePlayerResult,getPlayerMarker,updatePlayerMarker}
 }
-
-let playerOne = createPlayer("sufyan",1);
-let playerTwo = createPlayer("luffy",2);
-console.log(playerOne);
-console.log(playerTwo);
 let gameBoard = (function(){
-    let boardObj = {
-        "first":{"A1":1,"B1":2,"C1":3},
-        "second":{"A2":4,"B2":5,"C2":6},
-        "third":{"A3":7,"B3":8,"C3":9}
-    };
+    let boardObj 
+    let currentPlayer = null;
+    const updateCurrentPlayer = function (playerObj){
+        currentPlayer = playerObj;
+    }
+    const getCurrentPlayer = function (){
+        return currentPlayer;
+    }
+    const initializeBoard = function(){
+        boardObj = {
+            "first":{"A1":"","B1":"","C1":""},
+            "second":{"A2":"","B2":"","C2":""},
+            "third":{"A3":"","B3":"","C3":""}
+        };
+    }
     const updateBoard = function(rowNum,cellNum,value){
         let toUpdate = checkValid(rowNum,cellNum);
         if (toUpdate){
@@ -38,51 +43,53 @@ let gameBoard = (function(){
         
     }
     const checkValid = function (rowNum,cellNum){
-        if(boardObj[rowNum][cellNum] !== "X" || boardObj[rowNum][cellNum] !== "O" ){
-            return false;
-        }else{
+        if(boardObj[rowNum][cellNum] === ""){
             return true;
+        }else{
+            return false;
         }
     }
     const checkBoardWinner = function (playerone,playertwo){
-        let firstObj = boardObj["first"];
-        let secondObj = boardObj["second"];
-        let thirdObj = boardObj["third"];
+        let firstObj = Object.values(boardObj["first"]);
+        let secondObj = Object.values(boardObj["second"]);
+        let thirdObj = Object.values(boardObj["third"]);
         let winner = false;
         let playeroneMarker = playerone.getPlayerMarker();
         let playertwoMarker = playertwo.getPlayerMarker();
         let marker = "";
         let gameWinner = "";
-        if(firstObj["A1"] === firstObj["B1"] && firstObj["B1"] === firstObj["C1"]){
-            winner = true;
-            marker = firstObj["A1"];
-        } else if(secondObj["A2"] === secondObj["B2"] && secondObj["B2"] === secondObj["C2"]){
-            winner = true;
-            marker = secondObj["A2"]             
-        } else if(thirdObj["A3"] === thirdObj["B3"] && thirdObj["B3"] === thirdObj["C3"]){
-            winner = true;
-            marker = thirdObj["A3"]
-        } else if(firstObj["A1"] == secondObj["A2"] && secondObj["A2"] == thirdObj["A3"]){
-            winner = true;
-            marker = firstObj["A1"]
-        } else if(firstObj["B1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["B3"]){
-            winner = true;
-            marker = firstObj["B1"];
-        } else if(firstObj["C1"] == secondObj["C2"] && secondObj["C2"] == thirdObj["C3"]){
-            winner = true;
-            marker = firstObj["C1"]
-        } else if(firstObj["A1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["C3"]){
-            winner = true;
-            marker = firstObj["A1"]
-        } else if(firstObj["C1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["A3"]){
-            winner = true;
-            marker = firstObj["C1"]
-        }
+        if (!firstObj.includes("") && !secondObj.includes("") && !thirdObj.includes("")){
+            if(firstObj["A1"] === firstObj["B1"] && firstObj["B1"] === firstObj["C1"]){
+                winner = true;
+                marker = firstObj["A1"];
+            } else if(secondObj["A2"] === secondObj["B2"] && secondObj["B2"] === secondObj["C2"]){
+                winner = true;
+                marker = secondObj["A2"]             
+            } else if(thirdObj["A3"] === thirdObj["B3"] && thirdObj["B3"] === thirdObj["C3"]){
+                winner = true;
+                marker = thirdObj["A3"]
+            } else if(firstObj["A1"] == secondObj["A2"] && secondObj["A2"] == thirdObj["A3"]){
+                winner = true;
+                marker = firstObj["A1"]
+            } else if(firstObj["B1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["B3"]){
+                winner = true;
+                marker = firstObj["B1"];
+            } else if(firstObj["C1"] == secondObj["C2"] && secondObj["C2"] == thirdObj["C3"]){
+                winner = true;
+                marker = firstObj["C1"]
+            } else if(firstObj["A1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["C3"]){
+                winner = true;
+                marker = firstObj["A1"]
+            } else if(firstObj["C1"] == secondObj["B2"] && secondObj["B2"] == thirdObj["A3"]){
+                winner = true;
+                marker = firstObj["C1"]
+            }
+        };
         if(winner){
             if (marker === playeroneMarker){
-                gameWinner = playerOne.name;
+                gameWinner = playerone.name;
             } else if(marker === playertwoMarker){
-                gameWinner = playerTwo.name;
+                gameWinner = playertwo.name;
             }
         }
         return {winner,gameWinner}
@@ -94,7 +101,110 @@ let gameBoard = (function(){
         let tie = (firstObj.includes("X","O") && secondObj.includes("X","O") && thirdObj.includes("X","O"));
         return tie;
     }
-    return {updateBoard,checkValid,checkBoardWinner,checkBoardTie}
+
+    function printBoard(){
+        console.log(boardObj.first.A1,"|",boardObj.first.B1,"|",boardObj.first.C1);
+        console.log("_","_","_");
+        console.log(boardObj.second.A2,"|",boardObj.second.B2,"|",boardObj.second.C2);
+        console.log("_","_","_");
+        console.log(boardObj.third.A3,"|",boardObj.third.B3,"|",boardObj.third.C3);
+    }
+    return {updateBoard,checkValid,checkBoardWinner,checkBoardTie,initializeBoard,updateCurrentPlayer,getCurrentPlayer,printBoard}
 })();
-let tie = gameBoard.checkBoardTie();
-let winner = gameBoard.checkBoardWinner(playerOne,playerTwo);
+
+function controller(board){
+    console.log
+    let boardRowTrans = {
+        "1":"first",
+        "2":"second",
+        "3":"third"
+    }
+    const boardCellTrans = {
+        "first" : {"1":"A1","2":"B1","3":"C1"},
+        "second" : {"1":"A2","2":"B2","3":"C2"},
+        "third" : {"1":"A3","2":"B3","3":"C3"}
+    }
+    
+    const getPlayerInput = function(){
+        let validInput = false;
+        let currPlayerName = board.getCurrentPlayer().name;
+        let currPlayerMarker = board.getCurrentPlayer().getPlayerMarker();
+        while (!validInput) {
+            let msg = currPlayerName+" please play your turn again, your marker is "+currPlayerMarker+" (eg.input: row,cell,value .i.e. 1,2,"+currPlayerMarker+") please enter your input here: ";
+            let playerInput = prompt(msg);
+            let playerInputArr = playerInput.split(",")
+            validInput = playerInputArr.length == 3 ? true : false;
+            if (validInput){
+                let row = boardRowTrans[playerInputArr[0]];
+                let cell = undefined;
+                if (row){
+                    cell = boardCellTrans[row][playerInputArr[1]];
+                }
+                let value = playerInputArr[2].toUpperCase();
+                
+                let validVals = (row && cell && value == currPlayerMarker) ? true : false;
+                if (!validVals){
+                    validInput = false;
+                } else if (validVals) {
+                    let validBoardEntry = board.checkValid(row,cell);
+                    if (validBoardEntry){
+                        board.updateBoard(row,cell,value);
+                    }
+                }
+            }
+        }
+        console.log(currPlayerName,"'s turn has been completed!");
+        board.printBoard();
+    }
+    return{getPlayerInput}
+}
+
+function playGame (){
+    let players = false;
+    var playerOne;
+    var playerTwo;
+    while (!players) {
+        if (playerOne && playerTwo){
+            players = true;
+        }
+        if (!playerOne){
+            playerOne = createPlayer(prompt("Player 1 please enter your name here and press enter: "),1);
+            playerOne.updatePlayerMarker("X");
+            console.log(playerOne.name, " created with marker, ",playerOne.getPlayerMarker());
+        }
+        if (!playerTwo){
+            playerTwo = createPlayer(prompt("Player 2 please enter your name here and press enter: "),2);
+            playerTwo.updatePlayerMarker("O");
+            console.log(playerTwo.name," created with marker, ",playerTwo.getPlayerMarker());
+        }
+    };
+    console.log("playerOne.getPlayerMarker()",playerOne)
+    gameBoard.initializeBoard();
+    console.log("GameBoard has been intialized.");
+    gameBoard.updateCurrentPlayer(playerOne);//by default first turn would be for player one.
+    gameBoard.printBoard();
+    let gameController = controller(gameBoard);
+    
+    while (!gameBoard.checkBoardWinner(playerOne,playerTwo).winner || !gameBoard.checkBoardTie()) {
+        let playerInTurn = gameBoard.getCurrentPlayer()
+        console.log("Currently player's ",playerInTurn.name," needs to play for ", playerInTurn.getPlayerMarker());
+        gameController.getPlayerInput();
+        if (gameBoard.getCurrentPlayer().number == 1){
+            gameBoard.updateCurrentPlayer(playerTwo);
+        } else{
+            gameBoard.updateCurrentPlayer(playerOne);
+        }
+    }
+
+    //once the code reaches this stage that means either the game is tied or else we have a winner.
+    let checkTie = gameBoard.checkBoardTie();
+    let winnerObj = gameBoard.checkBoardWinner(playerOne,playerTwo);
+    if(checkTie){
+        console.log("Hmm.. we have a tie try again.");
+        gameBoard.printBoard();
+    } else if(winnerObj.winner){
+        console.log("Congrats ", winnerObj.gameWinner, " you have won the game.!")
+        gameBoard.printBoard();
+    }
+}
+
